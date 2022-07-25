277a4a3cddcb88ef680c59a16eeb517d2093d482
Cedric Karns-Waters <nkarnswaters@gmail.com>
2022-07-25 16:25:32 -0400

    Move Horizontal Script Genericized

--- Assets/Scripts/Basic Mechanics/PlayerMovement.cs
+++ Assets/Scripts/Basic Mechanics/PlayerMovement.cs
@@ -5,19 +5,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed = 10f; //speed mult
 
-    private Rigidbody2D rb; //variable rb is for rigid body 
+    private Movement move = new Movement();
 
 
    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); //sets rb to be player rigidbody   
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal"); //horizontal movement along x axis
-	    rb.velocity = new Vector2(xMove, 0) * speed; //creates directional velocity
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Call movement script and move horizontal
+        move.MoveHorizontal(this.GetComponent<Rigidbody2D>(), Input.GetAxisRaw("Horizontal"), speed);
     }
 }
